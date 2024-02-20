1e8ce6bab8271ab4f72679ca5e4acbda6d175cd6
Frytos <frytos@protonmail.com>
2024-02-20 12:59:07 +0100

    corrects ex00 to a iterative function type

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,25 +6,29 @@
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 08:54:43 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/20 09:32:53 by argrouss         ###   ########.fr       */
+/*   Updated: 2024/02/20 12:58:15 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
+#include <stdlib.h>
 
 int	ft_iterative_factorial(int nb);
 
 int	ft_iterative_factorial(int nb)
 {
-	if (nb > 1)
-		return (nb * ft_iterative_factorial(nb - 1));
-	else if (nb < 1)
+	int	result;
+
+	if (nb < 1)
 		return (0);
 	else
-		return (1);
+		result = 1;
+	while (nb > 0)
+		result *= nb--;
+	return (result);
 }
 
-int	main(void)
-{
-	int nb = 1;
-	printf("fact : %d, result = %d\n", nb, ft_iterative_factorial(nb));
-}
+// int	main(int argc, char *argv[])
+// {
+// 	printf("fact : %d, result = %d\n", atoi(argv[argc - 1]),
+// 		ft_iterative_factorial(atoi(argv[argc - 1])));
+// }
