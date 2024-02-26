938d236efc2a92d03f0eafa701117faf059e907a
Frytos <frytos@protonmail.com>
2024-02-26 08:52:22 +0100

    split function for norminette

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -15,18 +15,42 @@
 #include <time.h>
 
 int	ft_sqrt(int nb);
+int	reduce_windows_and_get_sqrt(int nb, int floor, int ceiling, int explorer);
 
 int	ft_sqrt(int nb)
 {
-	int	i;
+	int	ceiling;
+	int	floor;
+	int	explorer;
 
-	if (nb <= 0)
+	if (nb <= 0 || 2147395600 < nb)
 		return (0);
-	i = 0;
-	while ((i * i) < nb && i <= 46340)
-		i++;
-	if (i * i == nb)
-		return (i);
+	else if (nb == 1)
+		return (1);
+	floor = 2;
+	ceiling = 46340;
+	explorer = 23171;
+	return (reduce_windows_and_get_sqrt(nb, floor, ceiling, explorer));
+}
+
+int	reduce_windows_and_get_sqrt(int nb, int floor, int ceiling, int explorer)
+{
+	while (ceiling - floor > 2)
+	{
+		explorer = (ceiling + floor) / 2;
+		if (explorer * explorer == nb)
+			return (explorer);
+		else if (explorer * explorer < nb)
+			floor = explorer;
+		else if (nb < explorer * explorer)
+			ceiling = explorer;
+	}
+	if ((floor + 1) * (floor + 1) == nb)
+		return (floor + 1);
+	else if (ceiling * ceiling == nb)
+		return (ceiling);
+	else if (floor * floor == nb)
+		return (floor);
 	else
 		return (0);
 }
@@ -37,25 +61,28 @@ int	ft_sqrt(int nb)
 //     printf("%d",ft_sqrt(atoi(argv[1])));
 // }
 
-int main(void)
-{
-	long n = 0;
-	int found;
-	clock_t t;
-	double time_taken;
-	clock_t t_launch;
-	t_launch = clock();
-	while (n < 2147395600)
-	{	
-		found = ft_sqrt(n++);
-		if (found)
-		{		
-			t = clock() - t;
-			time_taken = ((double)t)/CLOCKS_PER_SEC;
-			printf("\t%fs\t%fs\t#     %ld \t-> sqrt() ->\t %d     #\n", ((double)(clock() - t_launch))/CLOCKS_PER_SEC, time_taken, n, found);
-			t = clock();
-			n = n + 123456789;
-			printf("\n\tnew start\t\t\t#     %ld \n", n);
-		}
-	}
-}
+// int main(void)
+// {
+// 	long n = 0;
+// 	int found;
+// 	clock_t t;
+// 	double time_taken;
+// 	clock_t t_launch;
+// 	t_launch = clock();
+// 	while (n < 2147395600)
+// 	{	
+// 		n++;
+// 		found = ft_sqrt(n);
+// 		if (found)
+// 		{		
+// 			t = clock() - t;
+// 			time_taken = ((double)t)/CLOCKS_PER_SEC;
+// 			printf("\t%fs\t%fs\t#     %ld \t-> sqrt() ->\t %d     #\n"
+//				, ((double)(clock() - t_launch))/CLOCKS_PER_SEC
+//				, time_taken, n, found);
+// 			t = clock();
+// 			n = n + 123456789;
+// 			printf("\n\tnew start\t\t\t#     %ld \n", n);
+// 		}
+// 	}
+// }
